b79ad5c242a801ad11ecec36111e5206502ed799
Ramesh <Ramesh@gpsoft.com.au>
2013-07-14 00:21:12 +0545

    Added a new method named : GetNameByAccountType()

---
+++ include/createaccount.php
@@ -0,0 +1,176 @@
+<?php
+session_start();
+include_once 'sql_connection.php';
+?>
+<div align="center">
+    <form id="userDetailForm" method="POST" onsubmit="return RegisterUser()?true:false">
+            <div id="inputForAll">
+            <table>
+    	<tr>
+            <td width="144" valign="top" >
+          <label for="name">Name</label>            </td>
+            <td width="184" valign="top">
+            <div align="right">
+                <input  type="text" name="name" maxlength="50" size="30" value="" />
+          </div></td>
+        </tr>
+        <tr>
+            <td>Account Type</td>
+            <td>
+                <div align="right">
+                    <select id="roleList" name="roleList" class="listmenu" style="width: 100%"  onchange="ShowHidePatient()">
+                    <option selected="selected" value="0" >Select Role</option>
+                    <?php
+                        Logger::LogInformation("Loading Role List");
+                        $roleList = $sql->GetRoles($_SESSION['userid']);
+                        foreach ($roleList as $role) {
+                            echo "<option value='".$role[0]."'>".$role[1]."</option>";
+                        }
+                    ?>
+                    </select>
+                </div>            </td>
+        </tr>
+        <tr>
+            <td>Account Group</td>
+            <td>
+            <div align="right">
+                    <!-- <input name="training" type="text" size="60"/>-->
+                <select id="accountList" name="accountList" class="listmenu" style="width:100%">
+                    <option value="0" selected="selected">Select Account</option>
+                    <?php
+                        Logger::LogInformation("Loading AccountType List");
+                        $accountList = $sql->GetAccountList();
+                        foreach ($accountList as $account) {
+                            echo "<option value='".$account[0]."'>".$account[1]."</option>";
+                        }
+                    ?>
+                </select>
+            </div>            </td>
+        </tr>
+        <tr>
+            <td>Account Creation Date</td>
+            <td><div align="right">
+            <input type="date" value="<?php echo date('Y-m-d'); ?>"  name="creationdate" style="width:95%"/>
+            </div></td> 
+        </tr>   
+        <tr>
+        <td valign="top">
+        <label for="last_name">Street Address</label>        </td>
+        <td valign="top">
+        <div align="right">
+            <input  type="text" name="street" maxlength="50" size="30">
+            </div></td>
+        </tr>
+        <tr>
+        <td valign="top">
+        <label for="last_name">City</label>        </td>
+        <td valign="top">
+        <div align="right">
+            <input  type="text" name="City" maxlength="50" size="30">
+            </div></td>
+        </tr>
+        <tr>
+        <td valign="top">
+        <label for="last_name">State</label>        </td>
+        <td valign="top">
+        <div align="right">
+            <input  type="text" name="State" maxlength="50" size="30">
+            </div></td>
+        </tr>
+        <tr>
+        <td valign="top">
+        <label for="telephone">Phone Number</label>        </td>
+        <td valign="top">
+        <div align="right">
+            <input  type="text" name="telephone" maxlength="30" size="30">
+            </div></td>
+        </tr>
+        <tr>
+                                        <td>Birth Date</td>
+                                        <td><div align="right">
+                                                <input type="date" name="birthdate" style="width: 95%"/>			
+                                    </div></td> 
+              </tr>
+    </table>
+            </div>
+            <div id="inputForPatient">
+                <table>
+                    <tr>
+                        <td width="142" valign="top"><div align="left">Doctor</div></td>
+                      <td width="191" valign="top" style="text-align:right">
+<!--                          <input name="doctorName" type="text" size="30" maxlength="50" style="width: 100%"/>-->
+                          <select id="doctorList" name="doctorList" class="listmenu">
+                              <?php
+                                include_once 'sql_connection.php';
+                                 $docList = $sql->GetNameByAccountType("Doctor");
+                                 if(!$docList)
+                                     echo"<option value='0' selected='selected'>No Doctor Found<option>";
+                                 else{
+                                     for($i=0; $i<$docList.length; $i++){
+                                         echo"<option value='".$docList[$i][0]."' selected='selected'>".$docList[$i][1]."<option>";
+                                     }
+                                     Logger::LogInformation("Doctor List loaded");
+                                 }
+                              ?>
+                          </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center"><div align="left">Nurse</div></td>
+                      <td colspan="2" style="text-align:center" align="center">
+                          <input name="doctorName" type="text" style="width: 100%" size="30" maxlength="50"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center"><div align="left">Last Visit </div></td>
+                      <td colspan="2" style="text-align:center" align="center">
+                          <input name="doctorName" type="text" style="width: 100%" size="30" maxlength="50"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center"><div align="left">Purpose of Visit </div></td>
+                      <td colspan="2" style="text-align:center" align="center">
+                          <input name="doctorName" type="text" style="width: 100%" size="30" maxlength="50"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center"><div align="left">Diagnosis given </div></td>
+                      <td colspan="2" style="text-align:center" align="center">
+                          <input name="doctorName" type="text" style="width: 100%" size="30" maxlength="50"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center"><div align="left">Prescribed Medication </div></td>
+                      <td colspan="2" style="text-align:center" align="center">
+                          <input name="doctorName" type="text" style="width: 100%" size="30" maxlength="50"/>
+                      </td>
+                    </tr>
+<!--                    <tr>
+                      <td align="center"><input type="reset" />                      </td>
+                      <td colspan="2" style="text-align:center" align="center">
+                          <button type="submit" >Submit</button>
+                      </td>
+                    </tr>-->
+              </table>
+            </div>
+
+
+      <div id="buttons">
+        <table width="336">
+          <tr>
+            <td width="90" align="center"><div style="width:100%">
+              
+              <div align="right">
+                <input name="reset" type="reset" />
+                </div>
+            </div></td>
+            <td width="128" colspan="2" align="center" style="text-align:center">
+              <div align="right">
+                <input type="submit" />
+              </div></td>
+          </tr>
+        </table>
+      </div>
+    </form>
+ 
+</div>
